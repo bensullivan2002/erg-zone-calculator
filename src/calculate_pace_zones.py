57ce52f278017cd428fa4f87bd430b8e90020544
Ben Sullivan <bensullivan2002@gmail.com>
2025-07-10 10:01:39 +0100

    Added more tests and calculate_upper_bound_time_per_500m() implementation

--- src/calculate_pace_zones.py
+++ src/calculate_pace_zones.py
@@ -43,5 +43,31 @@ class PaceZoneCalculator:
             print(f"An unexpected error occurred: {e}")
             raise e
 
-    def calculate_upper_bound(self, zone, config):
+    def calculate_upper_bound_time_per_500m(
+        self, zone: str, config_file_path: str
+    ) -> float:
+        if not isinstance(zone, str):
+            raise TypeError("Invalid zone format - must be a string")
+        if not isinstance(config_file_path, str):
+            raise TypeError("Invalid config_file_path format - must be a string")
+        try:
+            with open(Path(config_file_path), "r") as f:
+                config = json.load(f)
+                upper_bound_coefficient = config[zone]["upper_bound"]
+                upper_bound_time_per_500m = 500 / (
+                    (2000 / self.two_km_time) * upper_bound_coefficient
+                )
+                return round(upper_bound_time_per_500m, self.precision)
+        except FileNotFoundError as e:
+            print(f"Config file not found at {config_file_path}")
+            raise e
+        except json.JSONDecodeError as e:
+            print(f"Invalid JSON format in config file at {config_file_path}")
+            raise e
+        except KeyError as e:
+            print(f"Invalid zone {zone} in config file at {config_file_path}")
+            raise e
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            raise e
         pass
--- test/test_calculate_pace_zones.py
+++ test/test_calculate_pace_zones.py
@@ -1,8 +1,22 @@
+import json
 from src.calculate_pace_zones import PaceZoneCalculator
 import pytest
 
 
 class TestPaceZoneCalculatorShould:
+    @pytest.mark.parametrize(
+        "two_km_time, precision, expected_exception",
+        [
+            (-1, 1, ValueError),
+            (1, -1, ValueError),
+            ("invalid", 1, TypeError),  # type: ignore
+            (400, "invalid", TypeError),  # type: ignore
+        ],
+    )
+    def test_reject_invalid_arguments_for_PaceZoneCalculator_constructor(self, two_km_time, precision, expected_exception):
+        with pytest.raises(expected_exception):
+            PaceZoneCalculator(two_km_time, precision)
+
     @pytest.mark.parametrize(
         "two_km_time,zone,expected_lower_bound, precision",
         [
@@ -24,14 +38,49 @@ class TestPaceZoneCalculatorShould:
         )
         assert lower_bound == expected_lower_bound
 
+    def test_reject_invalid_arguments_for_calculate_lower_bound_time_per_500m(self):
+        calculator = PaceZoneCalculator(400, 1)
+        with pytest.raises(TypeError):
+            calculator.calculate_lower_bound_time_per_500m(123, "config/pace_zones.json") # type: ignore
+        with pytest.raises(TypeError):
+            calculator.calculate_lower_bound_time_per_500m("UT2", 123) # type: ignore
+        with pytest.raises(FileNotFoundError):
+            calculator.calculate_lower_bound_time_per_500m("UT2", "invalid_path.json")
+        with pytest.raises(json.JSONDecodeError):
+            calculator.calculate_lower_bound_time_per_500m("UT3", "test/fixtures/invalid_pace_zones.json")
+        with pytest.raises(KeyError):
+            calculator.calculate_lower_bound_time_per_500m("INVALID_ZONE", "config/pace_zones.json")
+    
     @pytest.mark.parametrize(
-        "two_km_time, precision, expected_exception",
+        "two_km_time,zone,expected_upper_bound, precision",
         [
-            (-1, 1, ValueError),
-            ("invalid", 1, TypeError),  # type: ignore
-            (400, "invalid", TypeError),  # type: ignore
+            (415, "UT2", 128.1, 1),
+            (400, "UT1", 116.28, 2),
+            (450, "AT", 123.626, 3),
+            (415, "TR", 108.1, 1),
+            (415, "AC", 103.7, 1),
+            (415, "AP", 103.7, 1),
         ],
     )
-    def test_reject_calculation_for_invalid_inputs(self, two_km_time, precision, expected_exception):
-        with pytest.raises(expected_exception):
-            PaceZoneCalculator(two_km_time, precision)
+    def test_calculate_upper_bound_time_per_500m(
+        self, two_km_time, zone, expected_upper_bound, precision
+    ):
+        config_file_path = "config/pace_zones.json"
+        calculator = PaceZoneCalculator(two_km_time, precision)
+        upper_bound = calculator.calculate_upper_bound_time_per_500m(
+            zone, config_file_path
+        )
+        assert upper_bound == expected_upper_bound
+
+    def test_reject_invalid_arguments_for_calculate_upper_bound_time_per_500m(self):
+        calculator = PaceZoneCalculator(400, 1)
+        with pytest.raises(TypeError):
+            calculator.calculate_upper_bound_time_per_500m(123, "config/pace_zones.json") # type: ignore
+        with pytest.raises(TypeError):
+            calculator.calculate_upper_bound_time_per_500m("UT2", 123) # type: ignore
+        with pytest.raises(FileNotFoundError):
+            calculator.calculate_upper_bound_time_per_500m("UT2", "invalid_path.json")
+        with pytest.raises(json.JSONDecodeError):
+            calculator.calculate_upper_bound_time_per_500m("UT3", "test/fixtures/invalid_pace_zones.json")
+        with pytest.raises(KeyError):
+            calculator.calculate_upper_bound_time_per_500m("INVALID_ZONE", "config/pace_zones.json")
