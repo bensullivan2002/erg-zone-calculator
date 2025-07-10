15b54ef8f523c188ab3da29930d09b85117600fa
Ben Sullivan <bensullivan2002@gmail.com>
2025-07-10 16:17:20 +0100

    Removed unnecesary lines from stack and app files

--- infra/app.py
+++ infra/app.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-import os
-
 import aws_cdk as cdk
-
 from infra.infra_stack import InfraStack
 
 
@@ -20,7 +17,7 @@ InfraStack(app, "InfraStack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    env=cdk.Environment(account='971422671361', region='eu-west-2'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
--- infra/infra/infra_stack.py
+++ infra/infra/infra_stack.py
@@ -1,7 +1,5 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
 )
 from constructs import Construct
 
@@ -9,11 +7,4 @@ class InfraStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "InfraQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        pass
