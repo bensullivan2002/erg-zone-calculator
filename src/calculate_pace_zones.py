84e79ef5454803f76ed24854adb5dd712d325b16
Ben Sullivan <bensullivan2002@gmail.com>
2025-07-10 08:35:15 +0100

    Initial commit

---
+++ src/calculate_pace_zones.py
@@ -0,0 +1,43 @@
+import json
+from pathlib import Path
+
+
+class PaceZoneCalculator:
+    def __init__(self, two_km_time: float | int, precision: int) -> None:
+        if two_km_time <= 0:
+            raise ValueError("Invalid two_km_time - must be greater than 0")
+        if precision <= 0:
+            raise ValueError("Invalid precision - must be greater than 0")
+        if not isinstance(two_km_time, (float, int)):
+            raise TypeError("Invalid two_km_time format - must be a number")
+        if not isinstance(precision, int):
+            raise TypeError("Invalid precision format - must be an integer")
+        self.two_km_time = two_km_time
+        self.precision = precision
+
+    def calculate_lower_bound_time_per_500m(
+        self, zone: str, config_file_path: str
+    ) -> float:
+        try:
+            with open(Path(config_file_path), "r") as f:
+                config = json.load(f)
+                lower_bound_coefficient = config[zone]["lower_bound"]
+                lower_bound_time_per_500m = 500 / (
+                    (2000 / self.two_km_time) * lower_bound_coefficient
+                )
+                return round(lower_bound_time_per_500m, self.precision)
+        except FileNotFoundError as e:
+            print(f"Config file not found at {config_file_path}")
+            raise e
+        except json.JSONDecodeError as e:
+            print(f"Invalid JSON format in config file at {config_file_path}")
+            raise e
+        except KeyError as e:
+            print(f"Invalid zone {zone} in config file at {config_file_path}")
+            raise e
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            raise e
+
+    def calculate_upper_bound(self, zone, config):
+        pass
---
+++ test/test_calculate_pace_zones.py
@@ -0,0 +1,29 @@
+from src.calculate_pace_zones import PaceZoneCalculator
+import pytest
+
+
+class TestPaceZoneCalculatorShould:
+    @pytest.mark.parametrize(
+        "two_km_time,zone,expected_lower_bound, precision",
+        [
+            (415, "UT2", 128.1, 1),
+            (400, "UT1", 116.28, 2),
+            (450, "AT", 123.626, 3),
+            (415, "TR", 108.1, 1),
+            (415, "AC", 103.7, 1),
+            (415, "AP", 103.7, 1),
+        ],
+    )
+    def test_calculate_lower_bound_time_per_500m(
+        self, two_km_time, zone, expected_lower_bound, precision
+    ):
+        config_file_path = "config/pace_zones.json"
+        calculator = PaceZoneCalculator(two_km_time, precision)
+        lower_bound = calculator.calculate_lower_bound_time_per_500m(
+            zone, config_file_path
+        )
+        assert lower_bound == expected_lower_bound
+
+    def test_reject_calculation_for_negative_times(self):
+        with pytest.raises(ValueError):
+            PaceZoneCalculator(-1, 1)
