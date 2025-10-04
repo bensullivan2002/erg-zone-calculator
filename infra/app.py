7f584a89adebd25f87cfdc8c88de3ffc47c4dfaf
Ben Sullivan <bensullivan2002@gmail.com>
2025-10-04 15:37:07 +0100

    Restarting infra coding from scratch

--- infra/app.py
+++ infra/app.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
 from infra.infra_stack import InfraStack
+from infra.src.origin_S3_bucket import create_cdfn_bucket
 
 
 app = cdk.App()
 InfraStack(
     app,
     "InfraStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-    # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
     env=cdk.Environment(account="971422671361", region="eu-west-2"),
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+    cdfn_origin_s3_bucket = create_cdfn_bucket(app, "cdfn-bucket")
 )
 
 app.synth()
