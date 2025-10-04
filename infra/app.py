2dc2c8959a75871efc4f870a83a820f8e79722c1
Ben Sullivan <bensullivan2002@gmail.com>
2025-10-04 18:26:35 +0100

    Deployed S3 bucket for CloudFront origin

--- infra/app.py
+++ infra/app.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python3
-import aws_cdk as cdk
-from infra.infra_stack import InfraStack
-from infra.src.origin_S3_bucket import create_cdfn_bucket
+from aws_cdk import App, Stack, RemovalPolicy
+from aws_cdk import aws_s3 as s3
+from uuid import uuid4
+from constructs import Construct
 
 
-app = cdk.App()
-InfraStack(
-    app,
-    "InfraStack",
-    env=cdk.Environment(account="971422671361", region="eu-west-2"),
-    cdfn_origin_s3_bucket = create_cdfn_bucket(app, "cdfn-bucket")
-)
+class CdfnStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        uuid = str(uuid4())
+        bucket_id = f"CdfnBucket-{uuid}"
+        s3.Bucket(
+            self,
+            bucket_id,
+            access_control=s3.BucketAccessControl.PRIVATE,
+            auto_delete_objects=True,
+            cors=[
+                s3.CorsRule(
+                    allowed_methods=[s3.HttpMethods.GET],
+                    allowed_origins=["*"],
+                    allowed_headers=["*"],
+                    exposed_headers=[],
+                )
+            ],
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            enforce_ssl=True,
+            minimum_tls_version=1.2,
+            versioned=True,
+            removal_policy=RemovalPolicy.DESTROY,
+            website_index_document="index.html",
+            website_error_document="error.html",
+        )
+
+
+app = App()
+CdfnStack(app, "CdfnStack")
 
 app.synth()
